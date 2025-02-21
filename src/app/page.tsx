2a0c8ebab40995cd7b95176e9dc0f946c309aee8
nessed <ali.abid44444@gmail.com>
2025-02-21 23:16:06 +0500

    basic ui done now adding trainers json at 11pm 21 feb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,13 @@ import {
   Dropdown,
   DropdownMenu,
   DropdownItem,
+  Checkbox,
+  Pagination,
 } from "@heroui/react";
 import SearchBar from "@/components/SearchBar";
-import { EllipsisVertical } from 'lucide-react';
+import { EllipsisVertical, Search } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { Plus } from "lucide-react";
 
 import Image from "next/image";
 
@@ -219,7 +223,7 @@ export default function Home() {
       role: "Research Analyst",
       status: "Paused",
       actions: "",
-      avatar: "/blank-profilepic", // Default avatar
+      avatar: "/blank-profilepic.png", // Default avatar
     },
   ];
 
@@ -232,38 +236,103 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center min-h-screen bg-white p-4">
+      {/* Search bar with icon */}
       <div className="w-full flex flex-row justify-end gap-2 p-1">
-        {/* <SearchBar /> */}
-        <Input placeholder="Search" />
-        <Button className="font-semibold">Status</Button>
-        <Button className="font-semibold">Columns</Button>
-        <Button className="bg-secondary text-white font-semibold">
-          Add User
-        </Button>
-      </div>
-      <div className="flex flex-row justify-between gap-2 text-gray-600 p-2">
-        <div className="">Total 20 users</div>
-        <div>Rows per page: 5</div>
-      </div>
+        <Input
+          placeholder="Search by name..."
+          startContent={<Search className="text-gray-500" />}
+          className="p-2 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500"
+        />
 
-      <Table className="shadow-lg rounded-lg w-full">
-        <TableHeader className="font-bold w-screen ">
-          <TableColumn className="font-bold text-white bg-secondary">
-            ID
+        {/* Status dropdown */}
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className="font-semibold flex items-center gap-2">
+              Status
+              <ChevronDown className="text-gray-500" size={100} />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu className="text-gray-700">
+            <DropdownItem key="active">Active</DropdownItem>
+            <DropdownItem key="paused">Paused</DropdownItem>
+            <DropdownItem key="vacation">Vacation</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        {/* Columns dropdown */}
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className="font-semibold flex items-center gap-2">
+              Columns
+              <ChevronDown className="text-gray-500" size={100} />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu className="text-gray-700">
+            <DropdownItem key="id">ID</DropdownItem>
+            <DropdownItem key="avatar">Avatar</DropdownItem>
+            <DropdownItem key="name">Name</DropdownItem>
+            <DropdownItem key="role">Role</DropdownItem>
+            <DropdownItem key="status">Status</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        {/* Add User dropdown */}
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className="bg-secondary text-white font-semibold flex items-center justify-center gap-2">
+              Add User
+              <Plus className="text-white" size={100} />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu className="text-gray-700">
+            <DropdownItem key="add-new">Add New User</DropdownItem>
+            <DropdownItem key="import">Import Users</DropdownItem>
+            <DropdownItem key="export">Export Users</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+      {/* Rows per page with dropdown */}
+      <div className="flex flex-row justify-between gap-2 text-gray-600 p-2">
+        <div className="flex flex-row gap-1">
+          <div>Total:</div>
+          <div>20 users</div>
+        </div>
+        <div className="flex items-center gap-2">
+          <span>Rows per page:</span>
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="flat" size="sm" className="font-semibold">
+                5
+                <ChevronDown className="text-gray-500" size={16} />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu className="text-gray-700">
+              <DropdownItem key="5">5</DropdownItem>
+              <DropdownItem key="10">10</DropdownItem>
+              <DropdownItem key="15">15</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+      {/* Table displaying users */}
+      <Table className="shadow-lg rounded-lg w-full table-fixed">
+        <TableHeader className="font-bold w-screen">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
+            <Checkbox color="success" />
           </TableColumn>
-          <TableColumn className="font-bold text-white bg-secondary">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
             AVATAR
           </TableColumn>
-          <TableColumn className="font-bold text-white bg-secondary">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
             NAME
           </TableColumn>
-          <TableColumn className="font-bold text-white bg-secondary">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
             ROLE
           </TableColumn>
-          <TableColumn className="font-bold text-white bg-secondary">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
             STATUS
           </TableColumn>
-          <TableColumn className="font-bold text-white bg-secondary">
+          <TableColumn className="text-center font-bold text-white bg-secondary">
             ACTIONS
           </TableColumn>
         </TableHeader>
@@ -274,32 +343,48 @@ export default function Home() {
               className="text-gray-700 border-b border-opacity-100"
               key={user.id}
             >
-              <TableCell>{user.id}</TableCell>
+              <TableCell className="text-center">
+                <Checkbox color="success" />
+              </TableCell>
 
-              {/* Render the Avatar using next/image */}
-              <TableCell>
+              {/* Avatar */}
+              <TableCell className="text-center flex justify-center items-center">
                 <Image
-                  src={user.avatar} // Use the avatar URL (points to public/blank-profilepic.png)
+                  src={user.avatar}
                   alt={`${user.name}'s Avatar`}
-                  width={40} // Width of the image
-                  height={40} // Height of the image
-                  className="rounded-full" // Optional: make the image circular
+                  width={40}
+                  height={40}
+                  className="rounded-full"
                 />
               </TableCell>
 
-              <TableCell className="font-sans">{user.name}</TableCell>
-              <TableCell>{user.role}</TableCell>
-              <TableCell>
-                <Chip color={getStatusColor(user.status)}>{user.status}</Chip>
+              {/* User name */}
+              <TableCell className="text-center font-sans">
+                {user.name}
               </TableCell>
-              <TableCell>
+
+              {/* User role */}
+              <TableCell className="text-center">{user.role}</TableCell>
+
+              {/* Status */}
+              <TableCell className="text-center font-bold">
+                <Chip
+                  className="font-semibold "
+                  color={getStatusColor(user.status)}
+                >
+                  {user.status}
+                </Chip>
+              </TableCell>
+
+              {/* Actions */}
+              <TableCell className="text-center">
                 <Dropdown>
                   <DropdownTrigger>
                     <Button isIconOnly size="sm" variant="light">
-                      :{" "}
+                      <EllipsisVertical className="text-gray-500" />
                     </Button>
                   </DropdownTrigger>
-                  <DropdownMenu>
+                  <DropdownMenu className="text-gray-700">
                     <DropdownItem key="view">View</DropdownItem>
                     <DropdownItem key="edit">Edit</DropdownItem>
                     <DropdownItem key="delete">Delete</DropdownItem>
@@ -310,6 +395,18 @@ export default function Home() {
           ))}
         </TableBody>
       </Table>
+      <Pagination
+        key="secondary"
+        color="secondary"
+        className=" flex items-center justify-center py-5"
+        isCompact
+        loop
+        showControls
+        showShadow
+        initialPage={1}
+        total={10}
+      />
+      ;{" "}
     </div>
   );
 }
